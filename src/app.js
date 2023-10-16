657ae094599f254311c3173ed0567f6269025689
SebastianLl28 <llamucasebas@gmail.com>
2023-10-16 10:37:01 -0500

    feat: add cors

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 import express from 'express'
 // import morgan from 'morgan'
 import { homeRouter, categoryRouter } from './route/index.js'
+import cors from 'cors'
 
 const app = express()
 
+app.use(cors())
+
 app.set('view engine', 'pug')
 app.set('views', './src/view')
+
+// habilitar body parser
+// app.use(express.json())
+// app.use(express.urlencoded({ extended: true }))
 
 // configuration public folder
 app.use(express.static('src/public'))
