8f652f50cb28919ffe6cc0ad81e99407bb637f0c
SebastianLl28 <llamucasebas@gmail.com>
2023-10-16 21:02:17 -0500

    refactor: send the products from the server

--- src/controller/home.controller.js
+++ src/controller/home.controller.js
@@ -1,7 +1,10 @@
+import { findAllCategory } from '../service/category.service.js'
+
 /**
  * @param {import('express').Request} req
  * @param {import('express').Response} res
  */
-export const homeGet = (req, res) => {
-  res.render('page/home')
+export const homeGet = async (req, res) => {
+  const categories = await findAllCategory()
+  res.render('page/home', { categories })
 }
--- src/public/js/home.js
+++ src/public/js/home.js
@@ -1,36 +1,13 @@
-window.addEventListener('load', async () => {
-  await fetchingCategoies()
+// eslint-disable-next-line no-undef
+const splide = new Splide('.splide', {
+  type: 'loop',
+  gap: '1rem',
+  focus: 'start',
+  autoWidth: true,
+  perPage: 'auto',
+  arrows: false,
+  pagination: false,
+  drag: 'free'
 })
 
-/**
- * @param {Object[]} data
- */
-function showCategories (data) {
-  const categories = document.getElementById('categories_data')
-  categories.innerHTML = ''
-  data.forEach(category => {
-    categories.innerHTML += `
-    <div class="overflow-hidden w-14">
-      <div class="h-14 w-14 rounded-md bg-gray-100 flex items-center justify-center py-2">
-        <img src="${category.image}" alt="${category.name}" class="h-12 bg-cover object-cover" />
-      </div>
-      <p class="text-ellipsis overflow-hidden text-center px-2 text-sm">${category.name}</p>
-    </div>
-    `
-  })
-}
-
-async function fetchingCategoies () {
-  try {
-    const response = await fetch('http://localhost:8001/api/category')
-    if (!response.ok) {
-      throw new Error('Network response was not ok')
-    }
-    const data = await response.json()
-    showCategories(data)
-  } catch (error) {
-    console.error('Error:', error)
-  }
-}
-
-// fetching products
+splide.mount()
