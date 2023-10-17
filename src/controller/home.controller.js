18381637c1f12709c7570e11d004391648d6f2c2
SebastianLl28 <llamucasebas@gmail.com>
2023-10-16 23:32:19 -0500

    feat: add carrousel banner and product

--- src/controller/home.controller.js
+++ src/controller/home.controller.js
@@ -1,10 +1,12 @@
 import { findAllCategory } from '../service/category.service.js'
+import { findAllProducts } from '../service/product.service.js'
 
 /**
  * @param {import('express').Request} req
  * @param {import('express').Response} res
  */
 export const homeGet = async (req, res) => {
+  const products = await findAllProducts({ limit: 8, order: [['createdAt', 'DESC']], attributes: ['name', 'description', 'price', 'image'] })
   const categories = await findAllCategory()
-  res.render('page/home', { categories })
+  res.render('page/home', { categories, products })
 }
--- src/public/js/home.js
+++ src/public/js/home.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-undef
-const splide = new Splide('.splide', {
+const splide = new Splide('.splide-categories', {
   type: 'loop',
   gap: '1rem',
   focus: 'start',
@@ -11,3 +11,34 @@ const splide = new Splide('.splide', {
 })
 
 splide.mount()
+
+// eslint-disable-next-line no-undef
+const splideNewProducts = new Splide('.splide-new-products', {
+  type: 'loop',
+  gap: '1rem',
+  focus: 'start',
+  autoWidth: true,
+  perPage: 'auto',
+  arrows: false,
+  pagination: false,
+  drag: 'free'
+})
+
+splideNewProducts.mount()
+
+// eslint-disable-next-line no-undef
+const splideNewBanner = new Splide('.splide-new-banner', {
+  perPage: 1,
+  type: 'loop',
+  gap: '1rem',
+  focus: 'start',
+  autoHeight: true,
+  autoWidth: true,
+  arrows: false,
+  pagination: true,
+  autoplay: true
+}).mount()
+
+splideNewBanner.on('click', (e) => {
+  console.log(e)
+})
