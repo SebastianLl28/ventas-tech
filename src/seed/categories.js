d2296c9fced91c37033fa906bdb3b373b193613f
SebastianLl28 <llamucasebas@gmail.com>
2023-10-16 20:58:39 -0500

    ci: add products to seeders

--- src/seed/categories.js
+++ src/seed/categories.js
@@ -4,12 +4,24 @@ const categories = [
     image: 'https://res.cloudinary.com/dhiogqtez/image/upload/v1697422622/image_tech/category/desktop-icon_gouogi.webp'
   },
   {
-    name: 'Laptop',
-    image: 'https://res.cloudinary.com/dhiogqtez/image/upload/v1697430925/image_tech/category/laptop-icon_rk2rpc.webp'
+    name: 'Other',
+    image: 'https://res.cloudinary.com/dhiogqtez/image/upload/v1697495695/image_tech/category/other-icon_dchevc.webp'
   },
   {
     name: 'Phone',
     image: 'https://res.cloudinary.com/dhiogqtez/image/upload/v1697431072/image_tech/category/phone-icon_mqdtlg.webp'
+  },
+  {
+    name: 'Tablet',
+    image: 'https://res.cloudinary.com/dhiogqtez/image/upload/v1697495505/image_tech/category/tablet-icon_aih6wc.webp'
+  },
+  {
+    name: 'Laptop',
+    image: 'https://res.cloudinary.com/dhiogqtez/image/upload/v1697430925/image_tech/category/laptop-icon_rk2rpc.webp'
+  },
+  {
+    name: 'Accessories',
+    image: 'https://res.cloudinary.com/dhiogqtez/image/upload/v1697495608/image_tech/category/accessories-icon_u0jqu5.webp'
   }
 ]
 
