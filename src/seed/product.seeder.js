8db7f737cac619f5c4f7e77023b1e750115b48f0
SebastianLl28 <llamucasebas@gmail.com>
2023-10-16 23:31:32 -0500

    feat: add seeder to populate product table

---
+++ src/seed/product.seeder.js
@@ -0,0 +1,100 @@
+/**
+ * @returns {Object[]}
+ */
+const products = [
+  {
+    name: 'Smartphone Samsung Galaxy S21',
+    description: 'The latest Samsung Galaxy S21 smartphone with high-resolution camera and powerful performance.',
+    price: 799.99,
+    stock: 50,
+    image: 'https://mipclista.com/4775-home_default/computadora-amd.jpg',
+    id_category: '6d523b45-17c8-4b29-b112-351866a54c2c'
+  },
+  {
+    name: 'Laptop Dell XPS 15',
+    description: 'The Dell XPS 15 laptop with a stunning 4K display and exceptional processing power.',
+    price: 1499.99,
+    stock: 30,
+    image: 'https://mipclista.com/4775-home_default/computadora-amd.jpg',
+    id_category: 'd8d1fc3a-7677-4b1a-8cc6-563876038f60'
+  },
+  {
+    name: 'Sony Noise-Canceling Headphones',
+    description: 'Immerse yourself in music with Sony\'s premium noise-canceling headphones.',
+    price: 249.99,
+    stock: 40,
+    image: 'https://mipclista.com/4775-home_default/computadora-amd.jpg',
+    id_category: '6d523b45-17c8-4b29-b112-351866a54c2c'
+  },
+  {
+    name: 'Smartwatch Apple Watch Series 7',
+    description: 'Stay connected and monitor your health with the Apple Watch Series 7.',
+    price: 349.99,
+    stock: 25,
+    image: 'https://mipclista.com/4775-home_default/computadora-amd.jpg',
+    id_category: '6d523b45-17c8-4b29-b112-351866a54c2c'
+  },
+  {
+    name: '4K Ultra HD TV 65-inch',
+    description: 'Experience stunning visuals with this 65-inch 4K Ultra HD television.',
+    price: 999.99,
+    stock: 15,
+    image: 'https://mipclista.com/4775-home_default/computadora-amd.jpg',
+    id_category: 'cc4fb4e4-32a5-453d-946b-1e433ef84c63'
+  },
+  {
+    name: 'HP OfficeJet Pro Printer',
+    description: 'Efficiently print, scan, and fax with the HP OfficeJet Pro all-in-one printer.',
+    price: 299.99,
+    stock: 20,
+    image: 'https://mipclista.com/4775-home_default/computadora-amd.jpg',
+    id_category: 'cc4fb4e4-32a5-453d-946b-1e433ef84c63'
+  },
+  {
+    name: 'Wireless Bluetooth Speaker',
+    description: 'Enjoy your favorite music wirelessly with this portable Bluetooth speaker.',
+    price: 49.99,
+    stock: 60,
+    image: 'https://mipclista.com/4775-home_default/computadora-amd.jpg',
+    id_category: '6d523b45-17c8-4b29-b112-351866a54c2c'
+  },
+  {
+    name: 'Gaming Laptop ASUS ROG Strix',
+    description: 'Unleash your gaming potential with the ASUS ROG Strix gaming laptop.',
+    price: 1899.99,
+    stock: 10,
+    image: 'https://mipclista.com/4775-home_default/computadora-amd.jpg',
+    id_category: 'd8d1fc3a-7677-4b1a-8cc6-563876038f60'
+  },
+  {
+    name: 'DJI Phantom 4 Pro Drone',
+    description: 'Capture stunning aerial footage with the DJI Phantom 4 Pro drone.',
+    price: 1499.99,
+    stock: 8,
+    image: 'https://mipclista.com/4775-home_default/computadora-amd.jpg',
+    id_category: 'e992fc4d-9da9-42dc-a4db-88944dd300a9'
+  },
+  {
+    name: 'Sony 65-inch OLED TV',
+    description: 'Experience cinematic visuals with the Sony 65-inch OLED television.',
+    price: 1899.99,
+    stock: 12,
+    image: 'https://mipclista.com/4775-home_default/computadora-amd.jpg',
+    id_category: 'cc4fb4e4-32a5-453d-946b-1e433ef84c63'
+  }
+]
+
+export default products
+
+/*
+  +--------------------------------------+-------------+
+  | id                                   | name        |
+  +--------------------------------------+-------------+
+  | 6d523b45-17c8-4b29-b112-351866a54c2c | Accessories |
+  | 925f3560-4de0-4723-8e10-e1b42a817eb3 | Phone       |
+  | b8ce0eaf-10c5-4562-bae8-599d4a624eb4 | Tablet      |
+  | cc4fb4e4-32a5-453d-946b-1e433ef84c63 | Desktop     |
+  | d8d1fc3a-7677-4b1a-8cc6-563876038f60 | Laptop      |
+  | e992fc4d-9da9-42dc-a4db-88944dd300a9 | Other       |
+  +--------------------------------------+-------------+
+*/
--- src/seed/seeder.js
+++ src/seed/seeder.js
@@ -1,5 +1,7 @@
-import categories from './categories.js'
-import Category from '../model/Category.js'
+// import categories from './categories.js'
+// import Category from '../model/Category.js'
+import products from './product.seeder.js'
+import Products from '../model/Product.js'
 import db from '../config/db.js'
 import { exit } from 'browser-sync'
 
@@ -7,7 +9,8 @@ const importarDatos = async () => {
   try {
     await db.authenticate()
     await db.sync()
-    await Category.bulkCreate(categories)
+    // await Category.bulkCreate(categories)
+    await Products.bulkCreate(products)
     console.log('Datos importados correctamente')
     exit()
   } catch (error) {
