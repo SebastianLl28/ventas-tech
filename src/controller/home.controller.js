f685cd8e7e138e86dbdc3e7dbf4d915a39313049
SebastianLl28 <llamucasebas@gmail.com>
2023-10-18 10:24:52 -0500

    refactor: Mejorar la estructura de la función homeGet * add try-catch block to handle errors * change from await to Promise for clarity

--- src/controller/home.controller.js
+++ src/controller/home.controller.js
@@ -6,10 +6,17 @@ import { Category } from '../model/index.js'
  * @param {import('express').Response} res
  */
 export const homeGet = async (req, res) => {
-  const products = await findAllProducts({ limit: 8, order: [['createdAt', 'DESC']], attributes: ['name', 'description', 'price', 'image'] })
-  const categories = await findAllCategory()
-  const laptops = await findAllProducts({ limit: 8, attributes: ['name', 'description', 'price', 'image'], include: { model: Category, where: { name: 'Laptop' } } })
-  const desks = await findAllProducts({ limit: 8, attributes: ['name', 'description', 'price', 'image'], include: { model: Category, where: { name: 'Desktop' } } })
-  const phons = await findAllProducts({ limit: 8, attributes: ['name', 'description', 'price', 'image'], include: { model: Category, where: { name: 'Phone' } } })
-  res.render('page/home', { categories, products, laptops, desks, phons })
+  try {
+    const [products, categories, laptops, desks, phons] = await Promise.all([
+      findAllProducts({ limit: 8, order: [['createdAt', 'DESC']], attributes: ['name', 'description', 'price', 'image'] }),
+      findAllCategory(),
+      findAllProducts({ limit: 8, attributes: ['name', 'description', 'price', 'image'], include: { model: Category, where: { name: 'Laptop' } } }),
+      findAllProducts({ limit: 8, attributes: ['name', 'description', 'price', 'image'], include: { model: Category, where: { name: 'Desktop' } } }),
+      findAllProducts({ limit: 8, attributes: ['name', 'description', 'price', 'image'], include: { model: Category, where: { name: 'Phone' } } })
+    ])
+    res.render('page/home', { categories, products, laptops, desks, phons })
+  } catch (error) {
+    console.log(error)
+    res.status(500).send('Internal Server Error')
+  }
 }
