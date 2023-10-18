920e96c67a14b9916ace8d2dbb3ddde35a3b1563
SebastianLl28 <llamucasebas@gmail.com>
2023-10-17 23:44:54 -0500

    feat: add computers, laptops and phones to the view, in carousels

--- src/controller/home.controller.js
+++ src/controller/home.controller.js
@@ -1,6 +1,6 @@
 import { findAllCategory } from '../service/category.service.js'
 import { findAllProducts } from '../service/product.service.js'
-
+import { Category } from '../model/index.js'
 /**
  * @param {import('express').Request} req
  * @param {import('express').Response} res
@@ -8,5 +8,8 @@ import { findAllProducts } from '../service/product.service.js'
 export const homeGet = async (req, res) => {
   const products = await findAllProducts({ limit: 8, order: [['createdAt', 'DESC']], attributes: ['name', 'description', 'price', 'image'] })
   const categories = await findAllCategory()
-  res.render('page/home', { categories, products })
+  const laptops = await findAllProducts({ limit: 8, attributes: ['name', 'description', 'price', 'image'], include: { model: Category, where: { name: 'Laptop' } } })
+  const desks = await findAllProducts({ limit: 8, attributes: ['name', 'description', 'price', 'image'], include: { model: Category, where: { name: 'Desktop' } } })
+  const phons = await findAllProducts({ limit: 8, attributes: ['name', 'description', 'price', 'image'], include: { model: Category, where: { name: 'Phone' } } })
+  res.render('page/home', { categories, products, laptops, desks, phons })
 }
--- src/public/js/home.js
+++ src/public/js/home.js
@@ -42,3 +42,50 @@ const splideNewBanner = new Splide('.splide-new-banner', {
 splideNewBanner.on('click', (e) => {
   console.log(e)
 })
+
+// eslint-disable-next-line no-undef
+const splideComputers = new Splide('.splide-computers', {
+  perPage: 1,
+  type: 'loop',
+  gap: '1rem',
+  focus: 'start',
+  autoHeight: true,
+  autoWidth: true,
+  arrows: false,
+  pagination: false
+}).mount()
+
+splideComputers.on('click', (e) => {
+  console.log(e)
+})
+
+// eslint-disable-next-line no-undef
+const splideLaptops = new Splide('.splide-laptops', {
+  perPage: 1,
+  type: 'loop',
+  gap: '1rem',
+  focus: 'start',
+  autoHeight: true,
+  autoWidth: true,
+  arrows: false,
+  pagination: false
+}).mount()
+
+splideLaptops.on('click', (e) => {
+  console.log(e)
+})
+// eslint-disable-next-line no-undef
+const splidePhone = new Splide('.splide-phone', {
+  perPage: 1,
+  type: 'loop',
+  gap: '1rem',
+  focus: 'start',
+  autoHeight: true,
+  autoWidth: true,
+  arrows: false,
+  pagination: false
+}).mount()
+
+splidePhone.on('click', (e) => {
+  console.log(e)
+})
